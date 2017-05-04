55da0cec357c75c00ac6df079d8ec966fddc23eb
hexing <he.xing@9douyu.com>
2017-05-04 11:20:38 +0800

    init

---
+++ init-as-user.sh
@@ -0,0 +1,31 @@
+function exe_cmd() {
+echo $1
+eval $1
+    }
+
+    function ensure_dir()
+    {
+        if [ ! -d $1 ]; then
+            exe_cmd "mkdir -p $1"
+        fi
+    }
+
+    function intall_basic_tools() {
+    if [ ! -d ~/git/work-anywhere/ ]; then
+        exe_cmd "cd ~/git"
+        exe_cmd "git clone https://github.com/liaohuqiu/work-anywhere.git"
+        exe_cmd "cd ~/git/work-anywhere/"
+        exe_cmd "sh tools/update-git-config.sh"
+    fi
+
+    if [ ! -d ~/git/vim_anywhere/ ]; then
+        exe_cmd "cd ~/git"
+        exe_cmd "git clone https://github.com/liaohuqiu/vim_anywhere.git"
+        exe_cmd "cd ~/git/vim_anywhere/"
+        exe_cmd "sh setup.sh"
+    fi
+
+}
+
+ensure_dir ~/git
+intall_basic_tools
